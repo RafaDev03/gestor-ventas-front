8d87b61f7d3fa0911959b825d2710ce5669ea88e
Daniel Gonzales <wtke90@gmail.com>
2024-05-03 11:27:26 -0500

    Mod Auth Part 1 Correccion de Bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,7 @@ import { ErrorComponent } from '~/shared/error/error.component';
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/login' },    
     { path: 'login', component: LoginComponent },
+    { path: 'home', loadChildren: () => import('./shared/home/home.routes').then(mod => mod.HomeRoutes), canActivate: [isLoggedInGuard] },  
     { path: '404', component: ErrorComponent }, 
-    { path: '**', redirectTo: '/404' },
-    {
-        path: 'home',
-        loadChildren: () => import('./shared/home/home.routes').then(mod => mod.HomeRoutes),
-        canActivate: [isLoggedInGuard]
-    },
+    { path: '**', redirectTo: '/404' }, 
 ];
